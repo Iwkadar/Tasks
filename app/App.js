a9474d5f8f22f8cc7e2328716f9cb2a726c799a0
Iwona <iwadar90@gmail.com>
2017-11-17 00:11:17 +0100

    added squares

--- app/App.js
+++ app/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <div className="container-fluid wrapper">
+        <div className="container">
           <Route exact path='/' component={c.Collection} />
         </div>
       </div>
